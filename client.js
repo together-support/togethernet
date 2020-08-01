76e950dd58ff78183bb8a27f619484f8a0e6ead4
Xin Xin <9159424+xinemata@users.noreply.github.com>
2020-08-01 02:02:29 -0700

    integrated basic interface

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const io = require("socket.io-client");
 const p5 = require("p5");
 const socket = io.connect(); // Manually opens the socket
 
-const url = "http://localhost:3000";
+const url = "https://togethernet-p2p-template.herokuapp.com";
 const archive = "/archive";
 
 // Simple Peer
@@ -13,9 +13,13 @@ let peers = {};
 
 // HTML elements
 let name = 'anonymous';
-let messageDiv; // where all messages display
+let privateMsg;
+let publicMsg;
 let messageInput; // text field to type message
 let sendBtn; // button to send message
+
+let publicMsgIndex = 0;
+let privateMsgIndex = 0;
 
 let incomingMsg;
 let outgoingMsg;
@@ -29,12 +33,11 @@ module.exports = new p5(function() {
         messageUI();
 
         // SOCKET.IO + SIMPLE PEER
-
         // Connects to the Node signaling server
         socket.on("connect", function() {
             // System broadcast
             let connectedMsg = `Connected to the signalling server`;
-            addMessage(connectedMsg);
+            addPrivateMsg(connectedMsg);
 
             // print your peer ID in the console
             console.log(`${connectedMsg}, your peer ID is ${socket.id}`);
@@ -46,12 +49,27 @@ module.exports = new p5(function() {
             // More API options here https://github.com/feross/simple-peer#peer--new-peeropts
             peer = new P2P({
                 initiator: data.initiator,
-                trickle: true
+                // reconnectTimer: 3000,
+                // iceTransportPolicy: 'relay',
+                trickle: false
+                    // config: {
+                    //     iceServers: [{
+                    //             urls: "stun:numb.viagenie.ca",
+                    //             username: "xinfilm@gmail.com",
+                    //             credential: "BerlinSpain"
+                    //         },
+                    //         {
+                    //             urls: "turn:numb.viagenie.ca",
+                    //             username: "xinfilm@gmail.com",
+                    //             credential: "BerlinSpain"
+                    //         }
+                    //     ]
+                    // }
             });
 
             // System broadcast
             let newPeerMsg = `You're available one the signal server but you have not been paired`;
-            addMessage(newPeerMsg);
+            addPrivateMsg(newPeerMsg);
             console.log(`${newPeerMsg} Peer ID: ${peerId}`);
 
             socket.on("signal", function(data) {
@@ -75,15 +93,15 @@ module.exports = new p5(function() {
             });
 
             peer.on("error", function(e) {
-                let errorMsg = `Error sending connection to peer: ${peerId}, ${e}`
-                addMessage(errorMsg);
-                console.log(errorMsg);
+                let errorMsg = `Oops, something went wrong. Try refreshing the page.`
+                addPrivateMsg(errorMsg);
+                console.log(`Error sending connection to peer: ${peerId}, ${e}`);
             });
 
             peer.on("connect", function() {
                 // System broadcast
                 let connectedPeerMsg = `Peer connection established. Say something.`;
-                addMessage(connectedPeerMsg);
+                addPrivateMsg(connectedPeerMsg);
                 console.log(`${connectedPeerMsg}`);
             });
 
@@ -92,7 +110,7 @@ module.exports = new p5(function() {
                 incomingMsg = data.toString();
 
                 // insert msg into the chatroom
-                addMessage(incomingMsg);
+                addPrivateMsg(incomingMsg);
 
                 console.log(`Recieved data from peer: ${incomingMsg}`);
             });
@@ -107,9 +125,11 @@ module.exports = new p5(function() {
 
 function messageUI() {
     // private msg HTML elements
-    messageDiv = document.querySelector("#_messageDiv"); // past messages go here
+    privateMsg = document.querySelector("#_privateMsg"); // past messages go here
+    publicMsg = document.querySelector("#_publicMsg"); // public messages go here
     messageInput = document.querySelector("#_messageInput"); // text input for message
     sendBtn = document.querySelector("#_sendBtn"); // send button
+    nameInput = document.querySelector("#_nameInput");
 
     // public msg HTML elements
     publicMsgInput = document.querySelector("#_publicMsgInput"); // text input for message
@@ -117,7 +137,7 @@ function messageUI() {
 
     // set events for sending message > trigger the sendMessage() function
     // -> for when button is blicked
-    sendBtn.addEventListener("click", sendPrivateMsg);
+    sendBtn.addEventListener("click", sendMessage);
     // -> for when "enter" is pressed in input field
     messageInput.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
@@ -125,35 +145,58 @@ function messageUI() {
             return false;
         }
     });
+}
 
-    publicSendBtn.addEventListener("click", sendPublicMsg);
-    // -> for when "enter" is pressed in input field
-    publicMsgInput.addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            sendPublicMsg();
-            return false;
+function sendMessage() {
+
+    if (nameInput.value != '') {
+        name = nameInput.value;
+    }
+
+    if (messageInput.value != '') {
+        outgoingMsg = `${name}: ${messageInput.value}`;
+        // send private message
+        if ($('.privateMsg').is(':visible') == true && $('.publicMsg').is(':visible') == false) {
+            // Send text/binary data to the remote peer. https://github.com/feross/simple-peer#peersenddata
+            peer.send(outgoingMsg);
+            addPrivateMsg(outgoingMsg);
         }
-    });
+        // send public message
+        else if ($('.publicMsg').is(':visible') == true && $('.privateMsg').is(':visible') == false) {
+            addPublicMsg(outgoingMsg);
+        }
+        // send private message
+        else if ($('.publicMsg').is(':visible') == true && $('.privateMsg').is(':visible') == true) {
+            peer.send(outgoingMsg);
+            addPrivateMsg(outgoingMsg);
+        }
+        console.log(`sending message: ${outgoingMsg}`); // note: using template literal string: ${variable} inside backticks
+        // clear input field
+        messageInput.value = "";
+    } else {
+        alert('your message is empty');
+    }
 }
 
-function sendPrivateMsg() {
-    // set text be value of input field
-    outgoingMsg = messageInput.value;
+function addPrivateMsg(data) {
 
-    // Send text/binary data to the remote peer. https://github.com/feross/simple-peer#peersenddata
-    peer.send(outgoingMsg);
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    console.log(`sending message: ${outgoingMsg}`); // note: using template literal string: ${variable} inside backticks
-
-    // insert msg into the chatroom
-    addMessage(outgoingMsg);
-
-    // clear input field
-    messageInput.value = "";
+    // add HTML to end of privateMsg
+    // the message is wrapped in a div with class "message" so it can be styled in CSS
+    privateMsg.insertAdjacentHTML(
+        "beforeend",
+        `<div class="message">
+      <p>${time} ${data}</p>
+  </div>`
+    );
+    // auto-scroll message container
+    privateMsg.scrollTop = privateMsg.scrollHeight - privateMsg.clientHeight;
 }
 
-function sendPublicMsg() {
-    outgoingPublicMsg = publicMsgInput.value;
+function addPublicMsg(outgoingPublicMsg) {
+
     let outgoingPublicJson = {
         author: name,
         msg: outgoingPublicMsg
@@ -172,25 +215,80 @@ function sendPublicMsg() {
         console.log(data);
     }); // posting url, object, 
 
-    // insert msg into the chatroom
-    addMessage(outgoingPublicMsg);
-
-    // clear input field
-    publicMsgInput.value = "";
-}
-
-function addMessage(data) {
     let today = new Date();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    // add HTML to end of messageDiv
+    publicMsgIndex++;
+
+    // add HTML to end of privateMsg
     // the message is wrapped in a div with class "message" so it can be styled in CSS
-    messageDiv.insertAdjacentHTML(
+    publicMsg.insertAdjacentHTML(
         "beforeend",
-        `<div class="message">
-        <p>${time} ${data}</p>
-    </div>`
+        `<div id="message${publicMsgIndex}">
+      <p>${time} ${outgoingPublicMsg}</p>
+  </div>`
     );
+
+    console.log(publicMsgIndex);
     // auto-scroll message container
-    messageDiv.scrollTop = messageDiv.scrollHeight - messageDiv.clientHeight;
+    publicMsg.scrollTop = publicMsg.scrollHeight - publicMsg.clientHeight;
 }
+
+// function sendPrivateMsg() {
+//     // set text be value of input field
+//     outgoingMsg = messageInput.value;
+
+//     // Send text/binary data to the remote peer. https://github.com/feross/simple-peer#peersenddata
+//     peer.send(outgoingMsg);
+
+//     console.log(`sending message: ${outgoingMsg}`); // note: using template literal string: ${variable} inside backticks
+
+//     // insert msg into the chatroom
+//     addMessage(outgoingMsg);
+
+//     // clear input field
+//     messageInput.value = "";
+// }
+
+// function sendPublicMsg() {
+//     outgoingPublicMsg = publicMsgInput.value;
+//     let outgoingPublicJson = {
+//         author: name,
+//         msg: outgoingPublicMsg
+//     }
+
+//     // send msg to archive/
+//     fetch(url + archive, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(outgoingPublicJson)
+//     }).then((res) => {
+//         return res.text();
+//     }).then((data) => {
+//         console.log(data);
+//     }); // posting url, object, 
+
+//     // insert msg into the chatroom
+//     addMessage(outgoingPublicMsg);
+
+//     // clear input field
+//     publicMsgInput.value = "";
+// }
+
+// function addMessage(data) {
+//     let today = new Date();
+//     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+//     // add HTML to end of privateMsg
+//     // the message is wrapped in a div with class "message" so it can be styled in CSS
+//     privateMsg.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class="message">
+//         <p>${time} ${data}</p>
+//     </div>`
+//     );
+//     // auto-scroll message container
+//     privateMsg.scrollTop = privateMsg.scrollHeight - privateMsg.clientHeight;
+// }
