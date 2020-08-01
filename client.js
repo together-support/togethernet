b879f85f8e526f49016e6b640588c6c9dfcf08a4
Xin Xin <9159424+xinemata@users.noreply.github.com>
2020-07-31 17:25:57 -0700

    connected to postgres
    sending archival messages from client.js to server.js

--- client.js
+++ client.js
@@ -4,6 +4,9 @@ const io = require("socket.io-client");
 const p5 = require("p5");
 const socket = io.connect(); // Manually opens the socket
 
+const url = "http://localhost:3000";
+const archive = "/archive";
+
 // Simple Peer
 let peer;
 let peers = {};
@@ -15,138 +18,172 @@ let sendBtn; // button to send message
 
 let incomingMsg;
 let outgoingMsg;
+let outgoingPublicMsg;
+let incomingPublicMsg;
 
 // P5.JS
 module.exports = new p5(function() {
-  this.setup = function setup() {
-    console.log("p5 is working");
-    messageUI();
+    this.setup = function setup() {
+        console.log("p5 is working");
+        messageUI();
 
-    // SOCKET.IO + SIMPLE PEER
+        // SOCKET.IO + SIMPLE PEER
 
-    // Connects to the Node signaling server
-    socket.on("connect", function() {
-      // System broadcast
-      let connectedMsg = `Connected to the signalling server`;
-      addMessage(connectedMsg);
-      
-      // print your peer ID in the console
-      console.log(`${connectedMsg}, your peer ID is ${socket.id}`);
-    });
+        // Connects to the Node signaling server
+        socket.on("connect", function() {
+            // System broadcast
+            let connectedMsg = `Connected to the signalling server`;
+            addMessage(connectedMsg);
 
-    socket.on("peer", function(data) {
-      let peerId = data.peerId;
-
-      // More API options here https://github.com/feross/simple-peer#peer--new-peeropts
-      peer = new P2P({
-        initiator: data.initiator,
-        trickle: true
-      });
-
-      // System broadcast
-      let newPeerMsg = `You're available one the signal server but you have not been paired`;
-      addMessage(newPeerMsg);
-      console.log(`${newPeerMsg} Peer ID: ${peerId}`);
-
-      socket.on("signal", function(data) {
-        if (data.peerId == peerId) {
-          console.log(
-            "Received signalling data",
-            data,
-            "from Peer ID:",
-            peerId
-          );
-          peer.signal(data.signal);
-        }
-      });
-
-      peer.on("signal", function(data) {
-        // Fired when the peer wants to send signaling data to the remote peer
-        socket.emit("signal", {
-          signal: data,
-          peerId: peerId
+            // print your peer ID in the console
+            console.log(`${connectedMsg}, your peer ID is ${socket.id}`);
         });
-      });
 
-      peer.on("error", function(e) {
-        let errorMsg = `Error sending connection to peer: ${peerId}, ${e}`
-        addMessage(errorMsg);
-        console.log(errorMsg);
-      });
+        socket.on("peer", function(data) {
+            let peerId = data.peerId;
 
-      peer.on("connect", function() {
-        // System broadcast
-        let connectedPeerMsg = `Peer connection established. Say something.`;
-        addMessage(connectedPeerMsg);
-        console.log(`${connectedPeerMsg}`);
-      });
+            // More API options here https://github.com/feross/simple-peer#peer--new-peeropts
+            peer = new P2P({
+                initiator: data.initiator,
+                trickle: true
+            });
 
-      peer.on("data", function(data) {
-        // converts received data from Unit8Array to string
-        incomingMsg = data.toString();
-        
-        // insert msg into the chatroom
-        addMessage(incomingMsg);
+            // System broadcast
+            let newPeerMsg = `You're available one the signal server but you have not been paired`;
+            addMessage(newPeerMsg);
+            console.log(`${newPeerMsg} Peer ID: ${peerId}`);
 
-        console.log(`Recieved data from peer: ${incomingMsg}`);
-      });
+            socket.on("signal", function(data) {
+                if (data.peerId == peerId) {
+                    console.log(
+                        "Received signalling data",
+                        data,
+                        "from Peer ID:",
+                        peerId
+                    );
+                    peer.signal(data.signal);
+                }
+            });
 
-      peers[peerId] = peer;
+            peer.on("signal", function(data) {
+                // Fired when the peer wants to send signaling data to the remote peer
+                socket.emit("signal", {
+                    signal: data,
+                    peerId: peerId
+                });
+            });
 
-      console.log(peers);
-    });
-  };
-  this.draw = function draw() {};
+            peer.on("error", function(e) {
+                let errorMsg = `Error sending connection to peer: ${peerId}, ${e}`
+                addMessage(errorMsg);
+                console.log(errorMsg);
+            });
+
+            peer.on("connect", function() {
+                // System broadcast
+                let connectedPeerMsg = `Peer connection established. Say something.`;
+                addMessage(connectedPeerMsg);
+                console.log(`${connectedPeerMsg}`);
+            });
+
+            peer.on("data", function(data) {
+                // converts received data from Unit8Array to string
+                incomingMsg = data.toString();
+
+                // insert msg into the chatroom
+                addMessage(incomingMsg);
+
+                console.log(`Recieved data from peer: ${incomingMsg}`);
+            });
+
+            peers[peerId] = peer;
+
+            console.log(peers);
+        });
+    };
+    this.draw = function draw() {};
 });
 
 function messageUI() {
-  // select HTML elements
-  messageDiv = document.querySelector("#_messageDiv"); // past messages go here
-  messageInput = document.querySelector("#_messageInput"); // text input for message
-  sendBtn = document.querySelector("#_sendBtn"); // send button
+    // private msg HTML elements
+    messageDiv = document.querySelector("#_messageDiv"); // past messages go here
+    messageInput = document.querySelector("#_messageInput"); // text input for message
+    sendBtn = document.querySelector("#_sendBtn"); // send button
 
-  // set events for sending message > trigger the sendMessage() function
-  // -> for when button is blicked
-  sendBtn.addEventListener("click", sendMessage);
-  // -> for when "enter" is pressed in input field
-  messageInput.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      sendMessage();
-      return false;
-    }
-  });
+    // public msg HTML elements
+    publicMsgInput = document.querySelector("#_publicMsgInput"); // text input for message
+    publicSendBtn = document.querySelector("#_publicSendBtn"); // send button
+
+    // set events for sending message > trigger the sendMessage() function
+    // -> for when button is blicked
+    sendBtn.addEventListener("click", sendMessage);
+    // -> for when "enter" is pressed in input field
+    messageInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            sendMessage();
+            return false;
+        }
+    });
+
+    publicSendBtn.addEventListener("click", sendPublicMsg);
+    // -> for when "enter" is pressed in input field
+    publicMsgInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            sendPublicMsg();
+            return false;
+        }
+    });
 }
 
 function sendMessage() {
-  // triggers when button or enter key is pressed
+    // triggers when button or enter key is pressed
 
-  // set text be value of input field
-  outgoingMsg = messageInput.value;
+    // set text be value of input field
+    outgoingMsg = messageInput.value;
 
-  // Send text/binary data to the remote peer. https://github.com/feross/simple-peer#peersenddata
-  peer.send(outgoingMsg);
+    // Send text/binary data to the remote peer. https://github.com/feross/simple-peer#peersenddata
+    peer.send(outgoingMsg);
 
-  console.log(`sending message: ${outgoingMsg}`); // note: using template literal string: ${variable} inside backticks
+    console.log(`sending message: ${outgoingMsg}`); // note: using template literal string: ${variable} inside backticks
 
-  // insert msg into the chatroom
-  addMessage(outgoingMsg);
+    // insert msg into the chatroom
+    addMessage(outgoingMsg);
 
-  // clear input field
-  messageInput.value = "";
+    // clear input field
+    messageInput.value = "";
+}
+
+function sendPublicMsg() {
+    outgoingPublicMsg = publicMsgInput.value;
+    let outgoingPublicJson = {
+        msg: outgoingPublicMsg
+    }
+    fetch(url + archive, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(outgoingPublicJson)
+    }).then((res) => {
+        return res.text();
+    }).then((data) => {
+        console.log(data);
+    }); // posting url, object, 
+    console.log(`sending public msg: ${outgoingPublicMsg}`);
 }
 
 function addMessage(data) {
-  let today = new Date();
-  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  
-  // add HTML to end of messageDiv
-  // the message is wrapped in a div with class "message" so it can be styled in CSS
-  messageDiv.insertAdjacentHTML(
-    "beforeend",
-    `<div class="message">
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    // add HTML to end of messageDiv
+    // the message is wrapped in a div with class "message" so it can be styled in CSS
+    messageDiv.insertAdjacentHTML(
+        "beforeend",
+        `<div class="message">
         <p>${time} ${data}</p>
     </div>`
-  );
-  // auto-scroll message container
-  messageDiv.scrollTop = messageDiv.scrollHeight - messageDiv.clientHeight;
+    );
+    // auto-scroll message container
+    messageDiv.scrollTop = messageDiv.scrollHeight - messageDiv.clientHeight;
 }
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const express = require("express");
 //Express:is used to host files on the server
 //You need a package.json to tie the dependencies to the server.js file
 const app = express();
+const bodyParser = require('body-parser');
 let server = http.Server(app);
 var io = require('socket.io')(server);
 
@@ -19,39 +20,48 @@ var io = require('socket.io')(server);
 
 // http://expressjs.com/en/starter/static-files.html
 var DEFAULT_PEER_COUNT = 5;
-app.use(express.static("public"));//Get files for static files
-app.get('/js/bundle.js', browserify(['debug', 'lodash', 'socket.io-client', 'simple-peer', 'p5', {'./client.js': {run: true}}]));
+app.use(express.static("public")); // use public folder as the location
+app.use(bodyParser.json());
+app.get('/js/bundle.js', browserify(['debug', 'lodash', 'socket.io-client', 'simple-peer', 'p5', { './client.js': { run: true } }]));
+app.post('/archive', (req, res) => { //request, response // app.post activates when something is posted to /archive
+    console.log(req.body);
+    res.send('hello');
+});
+app.get('/archive', (req, res) => {
+    res.send('got archive');
+});
 
-io.on('connection', function (socket) {
-  debug('Connection with ID:', socket.id);
-  var peersToAdvertise = _.chain(io.sockets.connected)
-    .values()
-    .without(socket)
-    .sampleSize(DEFAULT_PEER_COUNT)
-    .value();
-  debug('advertising peers', _.map(peersToAdvertise, 'id'));
-  peersToAdvertise.forEach(function(socket2) {
-    debug('Advertising peer %s to %s', socket.id, socket2.id);
-    socket2.emit('peer', {
-      peerId: socket.id,
-      initiator: true
+io.on('connection', function(socket) {
+    debug('Connection with ID:', socket.id);
+    console.log('Connected to node server');
+    var peersToAdvertise = _.chain(io.sockets.connected)
+        .values()
+        .without(socket)
+        .sampleSize(DEFAULT_PEER_COUNT)
+        .value();
+    debug('advertising peers', _.map(peersToAdvertise, 'id'));
+    peersToAdvertise.forEach(function(socket2) {
+        debug('Advertising peer %s to %s', socket.id, socket2.id);
+        socket2.emit('peer', {
+            peerId: socket.id,
+            initiator: true
+        });
+        socket.emit('peer', {
+            peerId: socket2.id,
+            initiator: false
+        });
     });
-    socket.emit('peer', {
-      peerId: socket2.id,
-      initiator: false
-    });
-  });
 
-  socket.on('signal', function(data) {
-    var socket2 = io.sockets.connected[data.peerId];
-    if (!socket2) { return; }
-    debug('Proxying signal from peer %s to %s', socket.id, socket2.id);
+    socket.on('signal', function(data) {
+        var socket2 = io.sockets.connected[data.peerId];
+        if (!socket2) { return; }
+        debug('Proxying signal from peer %s to %s', socket.id, socket2.id);
 
-    socket2.emit('signal', {
-      signal: data.signal,
-      peerId: socket.id
+        socket2.emit('signal', {
+            signal: data.signal,
+            peerId: socket.id
+        });
     });
-  });
 });
 
 server.listen(process.env.PORT || '3000');
