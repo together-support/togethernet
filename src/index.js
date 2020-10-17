9a1052026fc975a588674f37a45c1ba6ed2b5642
ywenc <cyaqingwen@gmail.com>
2020-10-17 11:19:08 -0400

    Env vars

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import browserify from 'browserify-middleware';
-import babelify from 'babelify';
+import browserify from '../browserify.js'
 
 import http from 'http';
 import path, {dirname} from 'path';
 import {fileURLToPath} from 'url';
 
 import {connectSocket} from './server/connection.js'
-
-browserify.settings({
-  transform: [babelify.configure({
-    extensions: ['.js'],
-    presets: ["@babel/preset-env", "@babel/preset-react"]
-  })]
-});
 
 dotenv.config();
 const app = express();
@@ -27,11 +19,7 @@ const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/js/bundle.js', browserify([
-  'debug', 'socket.io-client', 'simple-peer', 'p5', {
-    'src/client/index.js': { run: true }
-  }])
-);
+app.get('/js/bundle.js',  browserify('src/public/js/index.js'))
 
 const port = process.env.PORT || '3000';
 server.listen(port, () => console.log(`server listening on ${port}`));
