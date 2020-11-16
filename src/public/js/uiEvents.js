5e316b2d8da78d2c6ee70dcaded2d65d6bacca4c
cc <cc.wen@genius.com>
2020-11-15 21:11:39 -0500

    Send button is send options instead

--- src/public/js/uiEvents.js
+++ src/public/js/uiEvents.js
@@ -30,6 +30,10 @@ export const attachUIEvents = () => {
     store.sendToPeers({type: 'profileUpdated'});
   });
 
+  $('#changeMessageType').on('click', () => {
+    $('#messageTypes').show();
+  });
+
   new RoomForm().initialize();
 }
 
