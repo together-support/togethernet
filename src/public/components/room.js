f27d76c40819bbaa20a3a2b061c14a2049ea0662
CY Wen <cyaqingwen@gmail.com>
2020-11-21 19:00:36 -0500

    Room creator is preselected

--- src/public/components/room.js
+++ src/public/components/room.js
@@ -1,10 +1,14 @@
+import store from '../store/index.js';
+
 export const renderFacilitator = ({avatar, name, socketId}) => {
   return $(`<div class="facilitatorOption" data-socketId="${socketId}"><div style="background-color:${avatar}"></div><span>${name}</span></div>`)
 }
 
 export const facilitatorOption = ({profile, onClick}) => {
-  const {avatar, name} = profile
+  const {avatar, name, socketId} = profile
+  const isMe = store.getCurrentUser().getProfile().socketId === socketId;
   const option = $(`<button class="facilitatorOption"><div style="background-color:${avatar}"></div><span>${name}</span></button>`);
+  if (isMe) {option.addClass('selected');}
   option.on('click', (e) => {
     option.toggleClass('selected');
     onClick();
