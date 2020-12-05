4ca128b818b218d03bd8dfe16db1c03f8d77dbbe
CY Wen <cyaqingwen@gmail.com>
2020-12-05 16:32:05 -0500

    Basic display archived messages

--- src/public/js/archivalSpace.js
+++ src/public/js/archivalSpace.js
@@ -15,10 +15,34 @@ class ArchivalSpace {
 
   render () {
     this.messageRecords.forEach((messageRecord) => {
-      const {color} = messageRecord;
+      const {color, secondarycolors, author, content, participants, created_at} = messageRecord;
       const $messageRecordAvatar = $(`<div class="archival textRecord"></div>`);
-      $messageRecordAvatar.css({backgroundColor: color})
+      $messageRecordAvatar.css({backgroundColor: color});
+
+      const participantIndicatorSize = Math.round(50 / (Math.floor(Math.sqrt(secondarycolors.length)) + 1));
+      secondarycolors.forEach(secondarycolor => {
+        const $consentIndicator = $('<div class="consentIndicator" style="display:none"></div>');
+        $consentIndicator.css({backgroundColor: secondarycolor})
+        $consentIndicator.width(participantIndicatorSize);
+        $consentIndicator.height(participantIndicatorSize);
+        $consentIndicator.appendTo($messageRecordAvatar);
+      });
+
+      $messageRecordAvatar.mouseenter((e) => {
+        $(e.target).find('.consentIndicator').show();
+      }).mouseleave(e => {
+        $(e.target).find('.consentIndicator').hide();
+      });
+  
       $messageRecordAvatar.appendTo($('#archivalMessagesContainer'));
+
+      const $messageDetailsTemplate = $(document.getElementById('archivalMessagesDetailsTemplate').content.cloneNode(true));
+      const $messageDetails = $messageDetailsTemplate.find('.archivalMessagesDetails');
+      $messageDetails.find('.archivedTimestamp').text(new Date(created_at).toDateString())
+      $messageDetails.find('.participantNames').text(`Participants: ${participants.join(', ')}`)
+      $messageDetails.find('.author').text(author)
+      $messageDetails.find('.content').text(content)
+      $messageDetails.appendTo($('#archivalMessagesDetailsContainer'));
     });
   }
 }
