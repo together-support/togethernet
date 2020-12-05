2ffb5b070819a6f65c9b84c01f83d27928219e9f
CY Wen <cyaqingwen@gmail.com>
2020-12-05 17:15:07 -0500

    Use env vars

--- src/server/PGClient.js
+++ src/server/PGClient.js
@@ -6,11 +6,11 @@ const {Pool} = pg;
 class PGClient {
   constructor () {
     this.pool = new Pool({
-      user: 'yaqing',
-      host: 'localhost',
-      database: 'togethernet',
-      password: 'password',
-      port: 5432,    
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DB,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT,    
     });
   }
 
