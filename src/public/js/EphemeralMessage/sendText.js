c913db092a627c7224cb63f187333c86f51f3b64
CY Wen <cyaqingwen@gmail.com>
2021-02-01 20:15:38 -0500

    Comment on ephemeral vs archival

--- src/public/js/EphemeralMessage/sendText.js
+++ src/public/js/EphemeralMessage/sendText.js
@@ -9,32 +9,37 @@ export const sendMessage = () => {
     return;
   }
 
-  const gridColumnStart = $('#user .shadow').css('grid-column-start');
-  const gridRowStart = $('#user .shadow').css('grid-row-start');
-
   const currentRoom = store.getCurrentRoom();
-  const currentUser = store.getCurrentUser();
 
-  if ($(`#${currentUser.currentRoomId}-${gridColumnStart}-${gridRowStart}`).length) {
-    alert('move to an empty spot to write the msg');
-  }
-
-  const threadEntryMessageId = $('#writeMessage').attr('data-thread-entry-message');
-  const isPinned = $('#pinMessage').hasClass('clicked') && currentRoom.hasFacilitator(currentUser.socketId);
+  if (currentRoom.constructor.name === 'ArchivalSpace') {
+    return;
+  } else {
+    const gridColumnStart = $('#user .shadow').css('grid-column-start');
+    const gridRowStart = $('#user .shadow').css('grid-row-start');
   
-  const ephemeralMessage = new EphemeralMessage({
-    content, 
-    isPinned, 
-    gridColumnStart, 
-    gridRowStart, 
-    threadEntryMessageId, 
-    ...store.getCurrentUser().getProfile()
-  });
-
-  currentRoom.addEphemeralHistory(ephemeralMessage);
-  store.sendToPeers({type: 'text', data: ephemeralMessage.messageData});
-  ephemeralMessage.render();
-
-  $messageInput.val('');
-  $('#pinMessage').removeClass('clicked');
+    const currentUser = store.getCurrentUser();
+  
+    if ($(`#${currentUser.currentRoomId}-${gridColumnStart}-${gridRowStart}`).length) {
+      alert('move to an empty spot to write the msg');
+    }
+  
+    const threadEntryMessageId = $('#writeMessage').attr('data-thread-entry-message');
+    const isPinned = $('#pinMessage').hasClass('clicked') && currentRoom.hasFacilitator(currentUser.socketId);
+    
+    const ephemeralMessage = new EphemeralMessage({
+      content, 
+      isPinned, 
+      gridColumnStart, 
+      gridRowStart, 
+      threadEntryMessageId, 
+      ...store.getCurrentUser().getProfile()
+    });
+  
+    currentRoom.addEphemeralHistory(ephemeralMessage);
+    store.sendToPeers({type: 'text', data: ephemeralMessage.messageData});
+    ephemeralMessage.render();
+  
+    $messageInput.val('');
+    $('#pinMessage').removeClass('clicked');
+  }
 };
